49dc873a52b9e7ea21f9123bcacd944db5e6c49b
Andrea Jegher <andreaj-93@hotmail.it>
2016-11-07 20:35:16 +0100

    aggiunto player animator

--- CorriereHackaton2016/Assets/Scripts/Player.cs
+++ CorriereHackaton2016/Assets/Scripts/Player.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour {
 
     float m_horizontal;
     float m_vertical;
     Rigidbody2D m_rigid2d;
+    Animator m_anim;
     Transform tr;
 
     public float m_speed = 10f;
@@ -14,15 +16,14 @@ public class Player : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        tr = GetComponent<Transform>() as Transform;
-        m_rigid2d = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+        tr = GetComponent<Transform>();
+        m_rigid2d = gameObject.GetComponent<Rigidbody2D>();
+        m_anim = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-
         m_horizontal = Input.GetAxis("Horizontal");
-
         m_vertical = Input.GetAxis("Vertical");
     }
 
@@ -32,4 +33,12 @@ public class Player : MonoBehaviour {
                         + tr.up * m_vertical * m_speed;
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.collider.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Colpito");
+        }
+    }
 }
