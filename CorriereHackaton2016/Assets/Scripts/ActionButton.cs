c6491b34b9907b871506fd4f6678824cfd9f2065
redhotchilialex <redhotchilialex@gmail.com>
2016-11-07 17:00:23 +0100

    la meccanica del gioco cit.

---
+++ CorriereHackaton2016/Assets/Scripts/ActionButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionButton : MonoBehaviour {
+
+    public GameObject Next;
+
+	public void PopNext()
+    {
+        GameObject.Instantiate(Next);
+        GameObject.Destroy(gameObject);
+    }
+}
