c90c29ab488d113d3cd6aa91dd2b3b99a80fa680
Andrea Jegher <andreaj-93@hotmail.it>
2016-11-07 19:46:43 +0100

    Aggiunto controllo player

---
+++ CorriereHackaton2016/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+
+    float m_horizontal;
+    float m_vertical;
+    Rigidbody2D m_rigid2d;
+    Transform tr;
+
+    public float m_speed = 10f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        tr = GetComponent<Transform>() as Transform;
+        m_rigid2d = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_horizontal = Input.GetAxis("Horizontal");
+
+        m_vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        m_rigid2d.velocity = tr.right * m_horizontal * m_speed
+                        + tr.up * m_vertical * m_speed;
+    }
+
+}
