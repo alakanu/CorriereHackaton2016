185c90cdbe261095189e9c5f9519ea85129b1892
redhotchilialex <redhotchilialex@gmail.com>
2016-11-08 00:21:34 +0100

    up scena alessio

---
+++ CorriereHackaton2016/Assets/Scripts/Border.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Border : MonoBehaviour {
+
+    // Use this for initialization
+    void Awake()
+    {
+        EdgeCollider2D[] colliders = GetComponents<EdgeCollider2D>();
+
+        Bounds b = GetComponent<SpriteRenderer>().sprite.bounds;
+        Vector2 center = b.center;
+        Vector2 extents = b.extents;
+                
+        float x = extents.x;
+        float y = extents.y;
+        Vector2 topLeft = center + new Vector2 (-x, y);
+        Vector2 topRight = center + new Vector2 (x, y);
+        Vector2 bottomLeft = center + new Vector2 (-x, -y);
+        Vector2 bottomRight = center + new Vector2(x, -y);
+        Vector2[] upside = { topLeft, topRight };
+        Vector2[] downside = { bottomLeft, bottomRight };
+        Vector2[] leftside = { bottomLeft, topLeft };
+        Vector2[] rightside = { bottomRight, topRight };
+        colliders[0].points = upside;
+        colliders[1].points = downside;
+        colliders[2].points = leftside;
+        colliders[3].points = rightside;
+    }
+}
---
+++ CorriereHackaton2016/Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void OnDifficultyRise();
+    public static event OnDifficultyRise DifficultyRiseEvent;
+    
+}
