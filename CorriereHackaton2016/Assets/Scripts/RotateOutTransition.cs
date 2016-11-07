f4adc4db26433b9b7b88d1ee2ea4302c548bd391
redhotchilialex <redhotchilialex@gmail.com>
2016-11-07 18:50:56 +0100

    fmod setup

---
+++ CorriereHackaton2016/Assets/Scripts/RotateOutTransition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateOutTransition : MonoBehaviour {
+
+    protected Transform Pivot;
+    protected float acc;
+	
+	void Start ()
+    {
+        acc = 0;
+        Pivot = transform.Find("Pivot");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        acc += 45 * Time.deltaTime;
+        transform.RotateAround(Pivot.position, -Vector3.forward, 45 * Time.deltaTime);
+        if(acc >= 90)
+        {
+            Debug.Log("cacca");
+            gameObject.SetActive(false);
+        }
+	}
+}
