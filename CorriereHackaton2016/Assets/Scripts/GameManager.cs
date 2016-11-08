0bd3f1a6d0a73c8a790aff60a6db0ad8377b7f77
redhotchilialex <redhotchilialex@gmail.com>
2016-11-08 16:25:56 +0100

    ciaone

--- CorriereHackaton2016/Assets/Scripts/GameManager.cs
+++ CorriereHackaton2016/Assets/Scripts/GameManager.cs
@@ -7,7 +7,11 @@ public class GameManager : MonoBehaviour
 {
     protected Text Score;
 
+    protected Text Percentage;
+
     protected float time;
+
+    protected float perc;
 
     public delegate void OnNewsEvent();
     public static event OnNewsEvent NewsEvent;
@@ -20,6 +24,8 @@ public class GameManager : MonoBehaviour
         StartCoroutine(PushRandomNews());
         Player.PlayerDeath += ResetTime;
         time = 0;
+        perc = 100;
+        Percentage.text = "Percentage of vaccinated: 100%";
     }
 
     void Update()
@@ -36,7 +42,10 @@ public class GameManager : MonoBehaviour
     IEnumerator PushRandomNews()
     {
         yield return new WaitForSeconds(m_news_time);
-        if(NewsEvent != null)
+        perc = perc == 0 ? 0 : perc - 20;
+        
+        Percentage.text = "Percentage of vaccinated: " + perc+ " %";
+        if (NewsEvent != null)
             NewsEvent();
         StartCoroutine(PushRandomNews());
     }
